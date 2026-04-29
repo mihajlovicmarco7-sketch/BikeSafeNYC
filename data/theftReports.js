3e3d8baefd4ece38eb470913e18a7200c11e871f
mihajlovicmarco7-sketch <mihajlovicmarco7@gmail.com>
2026-04-29 17:20:18 -0400

    added theftReports data

---
+++ data/theftReports.js
@@ -0,0 +1,195 @@
+import { theftReports } from "../config/mongoCollections.js";
+import { ObjectId } from 'mongodb';
+import validation from '../helpers.js'
+
+const exportedMethods = {
+async createReport(
+    userId,
+    locationId,
+    locationName,
+    incidentDate,
+    bikeDescription,
+    contactEmail,
+    contactPhone,
+    notes) {
+  
+  // TODO: validation
+
+  let newReport = {
+    userId: new ObjectId(userId),
+    locationId: new ObjectId(locationId),
+    locationName: locationName,
+    incidentDate: new Date(incidentDate),
+    bikeDescription: bikeDescription,
+    contactEmail: contactEmail,
+    contactPhone: contactPhone,
+    notes: notes,
+    status: 'missing',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    comments: []
+  };
+
+  const theftReportsCollection = await theftReports();
+  const insertInfo = await theftReportsCollection.insertOne(newReport);
+  if (!insertInfo.acknowledged || !insertInfo.insertedId)
+    throw 'Could not add report';
+
+  const newId = insertInfo.insertedId.toString();
+  const data = await this.getTheftReportsById(newId);
+  
+  return data;
+},
+
+async getAllTheftReports(){
+  const theftReportsCollection = await theftReports();
+  const data = await theftReportsCollection.find({}).toArray();
+
+  data.forEach(elem => {
+    elem._id = elem._id.toString();
+    elem.userId = elem.userId.toString();
+    elem.locationId = elem.locationId.toString();
+
+    elem.comments.forEach(comment => {
+      comment._id = comment._id.toString();
+      comment.userId = comment.userId.toString();
+    });
+  });    
+
+  return data;
+},
+
+async getTheftReportsById(id){
+  id = validation.checkId(id);
+
+  const theftReportsCollection = await theftReports();
+  const data = await theftReportsCollection.findOne({_id: new ObjectId(id)});
+
+  if (!data) throw `Theft report ${id} not found!`;
+
+  data._id = data._id.toString();
+  data.userId = data.userId.toString();
+  data.locationId = data.locationId.toString();
+
+  data.comments.forEach(comment => {
+    comment._id = comment._id.toString();
+    comment.userId = comment.userId.toString();
+  });
+
+  return data;
+},
+
+async getReportsByLocation(id){
+  id = validation.checkId(id);
+
+  const theftReportsCollection = await theftReports();
+  const reports = await theftReportsCollection
+    .find({ locationId: new ObjectId(id) })
+    .toArray();
+  
+  reports.forEach(elem => {
+    elem._id = elem._id.toString();
+    elem.userId = elem.userId.toString();
+    elem.locationId = elem.locationId.toString();
+
+    elem.comments.forEach(comment => {
+      comment._id = comment._id.toString();
+      comment.userId = comment.userId.toString();
+    });
+  });
+
+  return reports;    
+},
+
+async getMissingReports(){
+  const theftReportsCollection = await theftReports();
+  const missingReports = await theftReportsCollection.find({ status: 'missing' }).toArray();
+
+  missingReports.forEach(elem => {
+    elem._id = elem._id.toString();
+    elem.userId = elem.userId.toString();
+    elem.locationId = elem.locationId.toString();
+
+    elem.comments.forEach(comment => {
+      comment._id = comment._id.toString();
+      comment.userId = comment.userId.toString();
+    });
+  });    
+  
+  return missingReports;
+},
+
+async updateReportStatus(id, newStatus){
+  id = validation.checkId(id);
+  if (!newStatus) throw new Error('status is required');
+  if (typeof newStatus !== 'string') throw new Error('Status must be a string');
+
+  const theftReportsCollection = await theftReports();
+
+  const updateInfo = await theftReportsCollection.updateOne(
+    { _id: new ObjectId(id) },
+    { $set: { status: newStatus.trim() } }
+  );
+
+  if (!updateInfo.matchedCount && !updateInfo.modifiedCount) {
+    throw new Error(`Could not update report with id of ${id}`);
+  }
+
+  const updatedReport = await theftReportsCollection.findOne({ _id: new ObjectId(id) });
+  
+  updatedReport._id = updatedReport._id.toString();
+  updatedReport.userId = updatedReport.userId.toString();
+  updatedReport.locationId = updatedReport.locationId.toString();
+
+  updatedReport.comments.forEach(comment => {
+    comment._id = comment._id.toString();
+    comment.userId = comment.userId.toString();
+  });
+  
+  return updatedReport;
+},
+
+async deleteReport(id){
+  id = validation.checkId(id);
+
+  const theftReportsCollection = await theftReports();
+  const data = await theftReportsCollection.findOneAndDelete({
+    _id: new ObjectId(id)
+  });
+
+  if (!data) {
+    throw `Could not report with ID of ${id}`;
+  }
+
+  return {id: id, deleted: true};
+
+},
+
+async getReportsByUser(userId){
+
+  userId = validation.checkId(userId);
+
+  const theftReportsCollection = await theftReports();
+
+  const userReports = await theftReportsCollection
+    .find({ userId: new ObjectId(userId) })
+    .toArray();
+
+  userReports.forEach(elem => {
+    elem._id = elem._id.toString();
+    elem.userId = elem.userId.toString();
+    elem.locationId = elem.locationId.toString();
+
+    elem.comments.forEach(comment => {
+      comment._id = comment._id.toString();
+      comment.userId = comment.userId.toString();
+    });
+  });    
+  
+  return userReports;
+}
+
+};
+
+export default exportedMethods;
+
