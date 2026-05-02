f2a222c08583cefc61df3d147846a646f20359ba
Eddie A Rodriguez <erodrigu3@stevens.edu>
2026-05-02 19:31:47 -0400

    Refactored dashboard route and updated related views and configs

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,25 @@
 import authRoutes from './authRoutes.js';
+import locationsRoutes from './locations.js';
 import reportsRoutes from './reports.js';
 import favoritesRoutes from './favorites.js';
-import locationsRoutes from './locations.js';
 
 const constructorMethod = (app) => {
-  // Root redirect
+  // Root redirects
   app.get('/', (req, res) => {
-    if (req.session.user) {
-      return res.redirect('/dashboard');
-    } else {
-      return res.redirect('/login');
-    }
+    return req.session.user
+      ? res.redirect('/dashboard')
+      : res.redirect('/login');
   });
 
-  // Auth routes (login + dashboard handled here)
+  // Auth routes (for login, signup, dashboard handled here)
   app.use('/', authRoutes);
 
-  // the other routes
+  // Feature routes
+  app.use('/locations', locationsRoutes);
   app.use('/reports', reportsRoutes);
   app.use('/favorites', favoritesRoutes);
-  app.use('/locations', locationsRoutes);
 
-  // 404 error
+  // 404 handles (last)
   app.use('*', (req, res) => {
     res.status(404).render('error', {
       title: '404',
