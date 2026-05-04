c4b332d11ab8341d4aabc68903bec3b8fa0ae212
Eddie A Rodriguez <erodrigu3@stevens.edu>
2026-05-04 15:13:01 -0400

    Clean PR - auth + user + views (new files only)

---
+++ middleware/auth.js
@@ -0,0 +1,12 @@
+export const requireLogin = (req, res, next) => {
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+  next();
+};
+export const redirectIfLoggedIn = (req, res, next) => {
+  if (req.session.user) {
+    return res.redirect('/dashboard');
+  }
+  next();
+};
